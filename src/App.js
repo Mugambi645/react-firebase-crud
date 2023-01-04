ce967a8de2c8b26ed2cc49ad1ff41ef6f17c5de7
mugambi645 <mugambi645@gmail.com>
2023-01-04 18:23:53 +0300

    styling changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark" className="header">
         <Container>
-          <Navbar.Brand href="#home">Library - Firebase CRUD</Navbar.Brand>
+          <Navbar.Brand href="#home">React Library - Firebase CRUD</Navbar.Brand>
         </Container>
       </Navbar>
 
